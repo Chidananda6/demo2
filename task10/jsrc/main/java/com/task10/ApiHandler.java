bf9e7da46ac843294c76bbda2840fb03b9182c50
Chidananda D A <chidananda_da@epam.com>
2024-08-09 15:04:45 +0530

    task10

--- task10/jsrc/main/java/com/task10/ApiHandler.java
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -1,250 +1,230 @@
 package com.task10;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
-import com.amazonaws.services.cognitoidp.model.AttributeType;
-import com.amazonaws.services.cognitoidp.model.AuthFlowType;
-import com.amazonaws.services.cognitoidp.model.InitiateAuthRequest;
-import com.amazonaws.services.cognitoidp.model.InitiateAuthResult;
-import com.amazonaws.services.cognitoidp.model.SignUpRequest;
-import com.amazonaws.services.cognitoidp.model.SignUpResult;
-import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
-import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
-import com.syndicate.deployment.model.RetentionSetting;
-import com.syndicate.deployment.model.ResourceType;
-import com.syndicate.deployment.annotations.resources.DependsOn;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.cognitoidp.model.*;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
-import com.amazonaws.services.dynamodbv2.model.ScanRequest;
-import com.amazonaws.services.dynamodbv2.model.ScanResult;
-import com.amazonaws.services.dynamodbv2.model.UpdateItemRequest;
-import com.amazonaws.services.dynamodbv2.model.UpdateItemResult;
+import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
+import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
+import com.syndicate.deployment.model.RetentionSetting;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.json.JSONObject;
-import org.json.JSONArray;
+import java.lang.UnsupportedOperationException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-@DependsOn(resourceType = ResourceType.COGNITO_USER_POOL, name = "simple-booking-userpool")
+import static com.syndicate.deployment.model.environment.ValueTransformer.USER_POOL_NAME_TO_CLIENT_ID;
+import static com.syndicate.deployment.model.environment.ValueTransformer.USER_POOL_NAME_TO_USER_POOL_ID;
+
+@DependsOn(resourceType = ResourceType.COGNITO_USER_POOL, name = "${booking_userpool}")
 @LambdaHandler(
 		lambdaName = "api_handler",
 		roleName = "api_handler-role",
 		isPublishVersion = false,
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@EnvironmentVariables(value = {
-		@EnvironmentVariable(key = "REGION", value = "eu-central-1"),
-		@EnvironmentVariable(key = "COGNITO_ID", value = "simple-booking-userpool"),
-		@EnvironmentVariable(key = "CLIENT_ID", value = "simple-booking-userpool")
+@EnvironmentVariables({
+		@EnvironmentVariable(key = "tables_table", value = "${tables_table}"),
+		@EnvironmentVariable(key = "reservations_table", value = "${reservations_table}"),
+		@EnvironmentVariable(key = "Cognito_ID", value = "${booking_userpool}", valueTransformer = USER_POOL_NAME_TO_USER_POOL_ID),
+		@EnvironmentVariable(key = "Client_ID", value = "${booking_userpool}", valueTransformer = USER_POOL_NAME_TO_CLIENT_ID)
 })
 public class ApiHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
-
 	private final AWSCognitoIdentityProvider cognitoClient;
-	private final AmazonDynamoDB dynamoDBClient;
 	private final DynamoDB dynamoDB;
+	private final String userPoolId;
 	private final String clientId;
+	private final Table tablesTable;
+	private final Table reservationsTable;
 
 	public ApiHandler() {
-		this.cognitoClient = AWSCognitoIdentityProviderClientBuilder.standard()
-				.withRegion(System.getenv("REGION"))
-				.build();
-
-		this.dynamoDBClient = AmazonDynamoDBClientBuilder.standard()
-				.withRegion(System.getenv("REGION"))
-				.build();
-
-		this.dynamoDB = new DynamoDB(dynamoDBClient);
-		this.clientId = System.getenv("CLIENT_ID");
+		cognitoClient = AWSCognitoIdentityProviderClientBuilder.standard().withRegion(System.getenv("region")).build();
+		dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.standard().build());
+		userPoolId = System.getenv("Cognito_ID");
+		clientId = System.getenv("Client_ID");
+		tablesTable = dynamoDB.getTable(System.getenv("tables_table"));
+		reservationsTable = dynamoDB.getTable(System.getenv("reservations_table"));
 	}
 
 	@Override
-	public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
-		String resource = (String) request.get("resource");
-		String httpMethod = (String) request.get("httpMethod");
+	public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
+		String path = (String) event.get("path");
+		String httpMethod = (String) event.get("httpMethod");
 		Map<String, Object> response = new HashMap<>();
-
 		try {
-			if ("/signup".equals(resource) && "POST".equals(httpMethod)) {
-				response = handleSignup(request);
-			} else if ("/signin".equals(resource) && "POST".equals(httpMethod)) {
-				response = handleSignin(request);
-			} else if ("/tables".equals(resource) && "GET".equals(httpMethod)) {
-				response = handleGetTables(request);
-			} else if ("/tables".equals(resource) && "POST".equals(httpMethod)) {
-				response = handleCreateTable(request);
-			} else if (resource.startsWith("/tables/") && "GET".equals(httpMethod)) {
-				response = handleGetTableById(request);
-			} else if ("/reservations".equals(resource) && "POST".equals(httpMethod)) {
-				response = handleCreateReservation(request);
-			} else if ("/reservations".equals(resource) && "GET".equals(httpMethod)) {
-				response = handleGetReservations(request);
+			if ("/signup".equals(path) && "POST".equals(httpMethod)) {
+				handleSignup(event, response);
+			} else if ("/signin".equals(path) && "POST".equals(httpMethod)) {
+				handleSignin(event, response);
+			} else if ("/tables".equals(path) && "GET".equals(httpMethod)) {
+				handleGetTables(response);
+			} else if ("/tables".equals(path) && "POST".equals(httpMethod)) {
+				handleCreateTable(event, response);
+			} else if (path.matches("/tables/\\d+") && "GET".equals(httpMethod)) {
+				handleGetTableById(path, response);
+			} else if ("/reservations".equals(path) && "POST".equals(httpMethod)) {
+				handleCreateReservation(event, response);
+			} else if ("/reservations".equals(path) && "GET".equals(httpMethod)) {
+				handleGetReservations(response);
 			} else {
-				response.put("statusCode", 400);
-				response.put("body", "Invalid request");
+				throw new UnsupportedOperationException("Unsupported operation: " + path + " " + httpMethod);
 			}
 		} catch (Exception e) {
-			response.put("statusCode", 500);
-			response.put("body", "Internal server error: " + e.getMessage());
+			response.put("statusCode", 400);
+			response.put("body", "{\"message\": \"Something went wrong: " + e.getMessage() + "\"}");
 		}
-
 		return response;
 	}
 
-	private Map<String, Object> handleSignup(Map<String, Object> request) {
-		JSONObject requestBody = new JSONObject((String) request.get("body"));
-		String firstName = requestBody.getString("firstName");
-		String lastName = requestBody.getString("lastName");
-		String email = requestBody.getString("email");
-		String password = requestBody.getString("password");
+	private void handleSignup(Map<String, Object> event, Map<String, Object> response) throws Exception {
+		Map<String, Object> body = parseRequestBody(event);
+		String email = (String) body.get("email");
+		String firstName = (String) body.get("firstName");
+		String lastName = (String) body.get("lastName");
+		String password = (String) body.get("password");
 
-		SignUpRequest signUpRequest = new SignUpRequest()
-				.withClientId(clientId)
+		AdminCreateUserRequest createUserRequest = new AdminCreateUserRequest()
+				.withUserPoolId(userPoolId)
+				.withUsername(email)
+				.withUserAttributes(
+						new AttributeType().withName("email").withValue(email),
+						new AttributeType().withName("firstname").withValue(firstName),
+						new AttributeType().withName("lastname").withValue(lastName))
+				.withTemporaryPassword(password)
+				.withMessageAction(MessageActionType.SUPPRESS);
+
+		cognitoClient.adminCreateUser(createUserRequest);
+
+		AdminSetUserPasswordRequest setUserPasswordRequest = new AdminSetUserPasswordRequest()
+				.withUserPoolId(userPoolId)
 				.withUsername(email)
 				.withPassword(password)
-				.withUserAttributes(
-						new AttributeType().withName("given_name").withValue(firstName),
-						new AttributeType().withName("family_name").withValue(lastName),
-						new AttributeType().withName("email").withValue(email)
-				);
+				.withPermanent(true);
 
-		SignUpResult signUpResult = cognitoClient.signUp(signUpRequest);
-
-		Map<String, Object> response = new HashMap<>();
+		cognitoClient.adminSetUserPassword(setUserPasswordRequest);
 		response.put("statusCode", 200);
-		response.put("body", "Sign-up process is successful");
-		return response;
+		response.put("body", "{\"message\": \"Sign-up process is successful\"}");
 	}
 
-	private Map<String, Object> handleSignin(Map<String, Object> request) {
-		JSONObject requestBody = new JSONObject((String) request.get("body"));
-		String email = requestBody.getString("email");
-		String password = requestBody.getString("password");
-
-		Map<String, String> authParams = new HashMap<>();
-		authParams.put("USERNAME", email);
-		authParams.put("PASSWORD", password);
+	private void handleSignin(Map<String, Object> event, Map<String, Object> response) throws Exception {
+		Map<String, Object> body = parseRequestBody(event);
+		String email = (String) body.get("email");
+		String password = (String) body.get("password");
 
 		InitiateAuthRequest authRequest = new InitiateAuthRequest()
-				.withClientId(clientId)
 				.withAuthFlow(AuthFlowType.USER_PASSWORD_AUTH)
-				.withAuthParameters(authParams);
+				.withAuthParameters(new HashMap<String, String>() {{
+					put("USERNAME", email);
+					put("PASSWORD", password);
+				}})
+				.withClientId(clientId);
 
 		InitiateAuthResult authResult = cognitoClient.initiateAuth(authRequest);
-		String idToken = authResult.getAuthenticationResult().getIdToken();
-
-		Map<String, Object> response = new HashMap<>();
+		String accessToken = authResult.getAuthenticationResult().getAccessToken();
 		response.put("statusCode", 200);
-		response.put("body", new JSONObject().put("accessToken", idToken).toString());
-		return response;
+		response.put("body", "{\"accessToken\": \"" + accessToken + "\"}");
 	}
 
-	private Map<String, Object> handleGetTables(Map<String, Object> request) {
-		Table table = dynamoDB.getTable("Tables");
-		ScanRequest scanRequest = new ScanRequest().withTableName("Tables");
-		ScanResult result = dynamoDBClient.scan(scanRequest);
+	private void handleGetTables(Map<String, Object> response) throws Exception {
+		ItemCollection<ScanOutcome> items = tablesTable.scan(new ScanSpec());
+		List<Item> itemList = new ArrayList<>();
+		items.forEach(itemList::add);
+		itemList.sort(Comparator.comparing(item -> item.getInt("id")));
 
-		JSONArray tables = new JSONArray();
-		result.getItems().forEach(item -> tables.put(new JSONObject(item)));
-
-		Map<String, Object> response = new HashMap<>();
+		ObjectMapper mapper = new ObjectMapper();
+		String body = mapper.writeValueAsString(Collections.singletonMap("tables", itemList));
 		response.put("statusCode", 200);
-		response.put("body", new JSONObject().put("tables", tables).toString());
-		return response;
+		response.put("body", body);
 	}
 
-	private Map<String, Object> handleCreateTable(Map<String, Object> request) {
-		JSONObject requestBody = new JSONObject((String) request.get("body"));
-		String id = requestBody.getString("id");
-		String number = requestBody.getString("number");
-		String places = requestBody.getString("places");
-		String isVip = requestBody.getString("isVip");
-		String minOrder = requestBody.optString("minOrder");
+	private void handleCreateTable(Map<String, Object> event, Map<String, Object> response) throws Exception {
+		Map<String, Object> body = parseRequestBody(event);
+		Item item = new Item()
+				.withPrimaryKey("id", body.get("id"))
+				.withString("number", (String) body.get("number"))
+				.withString("places", (String) body.get("places"))
+				.withBoolean("isVip", (Boolean) body.get("isVip"))
+				.withNumber("minOrder", (Number) body.get("minOrder"));
 
-		Map<String, AttributeValue> item = new HashMap<>();
-		item.put("id", new AttributeValue(id));
-		item.put("number", new AttributeValue(number));
-		item.put("places", new AttributeValue(places));
-		item.put("isVip", new AttributeValue(isVip));
-		if (minOrder != null && !minOrder.isEmpty()) {
-			item.put("minOrder", new AttributeValue(minOrder));
+		tablesTable.putItem(item);
+		response.put("statusCode", 200);
+		response.put("body", "{\"id\": \"" + body.get("id") + "\"}");
+	}
+
+	private void handleGetTableById(String path, Map<String, Object> response) throws Exception {
+		int tableId = Integer.parseInt(path.split("/")[2]);
+		Item item = tablesTable.getItem("id", tableId);
+		if (item != null) {
+			ObjectMapper mapper = new ObjectMapper();
+			String body = mapper.writeValueAsString(item.asMap());
+			response.put("statusCode", 200);
+			response.put("body", body);
+		} else {
+			response.put("statusCode", 404);
+			response.put("body", "{\"message\": \"Table not found\"}");
+		}
+	}
+
+	private void handleCreateReservation(Map<String, Object> event, Map<String, Object> response) throws Exception {
+		Map<String, Object> body = parseRequestBody(event);
+		int tableNumber = (int) body.get("tableNumber");
+		String reservationDate = (String) body.get("date");
+		String slotTimeStart = (String) body.get("slotTimeStart");
+		String slotTimeEnd = (String) body.get("slotTimeEnd");
+		SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+		Date proposedTimeStart = timeFormat.parse(slotTimeStart);
+		Date proposedTimeEnd = timeFormat.parse(slotTimeEnd);
+		ItemCollection<ScanOutcome> reservations = reservationsTable.scan(new ScanSpec());
+
+		for (Item item : reservations) {
+			if (tableNumber == item.getInt("tableNumber") &&
+					reservationDate.equals(item.getString("date"))) {
+				Date bookedStartTime = timeFormat.parse(item.getString("slotTimeStart"));
+				Date bookedEndTime = timeFormat.parse(item.getString("slotTimeEnd"));
+				if (!(proposedTimeEnd.before(bookedStartTime) || proposedTimeStart.after(bookedEndTime))) {
+					response.put("statusCode", 400);
+					response.put("body", "{\"message\": \"Table already booked for this time slot.\"}");
+					return;
+				}
+			}
 		}
 
-		PutItemRequest putItemRequest = new PutItemRequest()
-				.withTableName("Tables")
-				.withItem(item);
-
-		dynamoDBClient.putItem(putItemRequest);
-
-		Map<String, Object> response = new HashMap<>();
+		Item reservation = new Item()
+				.withPrimaryKey("tableNumber", tableNumber)
+				.withString("date", reservationDate)
+				.withString("slotTimeStart", slotTimeStart)
+				.withString("slotTimeEnd", slotTimeEnd)
+				.withString("userId", (String) body.get("userId"));
+		reservationsTable.putItem(reservation);
 		response.put("statusCode", 200);
-		response.put("body", new JSONObject().put("id", id).toString());
-		return response;
+		response.put("body", "{\"message\": \"Reservation successful\"}");
 	}
 
-	private Map<String, Object> handleGetTableById(Map<String, Object> request) {
-		String tableId = request.get("pathParameters").toString();
+	private void handleGetReservations(Map<String, Object> response) throws Exception {
+		ItemCollection<ScanOutcome> items = reservationsTable.scan(new ScanSpec());
+		List<Item> itemList = new ArrayList<>();
+		items.forEach(itemList::add);
 
-		GetItemSpec spec = new GetItemSpec().withPrimaryKey("id", tableId);
-
-		Table table = dynamoDB.getTable("Tables");
-		JSONObject item = new JSONObject(table.getItem(spec).toJSON());
-
-		Map<String, Object> response = new HashMap<>();
+		ObjectMapper mapper = new ObjectMapper();
+		String body = mapper.writeValueAsString(Collections.singletonMap("reservations", itemList));
 		response.put("statusCode", 200);
-		response.put("body", item.toString());
-		return response;
+		response.put("body", body);
 	}
 
-	private Map<String, Object> handleCreateReservation(Map<String, Object> request) {
-		JSONObject requestBody = new JSONObject((String) request.get("body"));
-		String tableNumber = requestBody.getString("tableNumber");
-		String clientName = requestBody.getString("clientName");
-		String phoneNumber = requestBody.getString("phoneNumber");
-		String date = requestBody.getString("date");
-		String slotTimeStart = requestBody.getString("slotTimeStart");
-		String slotTimeEnd = requestBody.getString("slotTimeEnd");
-
-		String reservationId = java.util.UUID.randomUUID().toString();
-		Map<String, AttributeValue> item = new HashMap<>();
-		item.put("reservationId", new AttributeValue(reservationId));
-		item.put("tableNumber", new AttributeValue(tableNumber));
-		item.put("clientName", new AttributeValue(clientName));
-		item.put("phoneNumber", new AttributeValue(phoneNumber));
-		item.put("date", new AttributeValue(date));
-		item.put("slotTimeStart", new AttributeValue(slotTimeStart));
-		item.put("slotTimeEnd", new AttributeValue(slotTimeEnd));
-
-		PutItemRequest putItemRequest = new PutItemRequest()
-				.withTableName("Reservations")
-				.withItem(item);
-
-		dynamoDBClient.putItem(putItemRequest);
-
-		Map<String, Object> response = new HashMap<>();
-		response.put("statusCode", 200);
-		response.put("body", new JSONObject().put("reservationId", reservationId).toString());
-		return response;
-	}
-
-	private Map<String, Object> handleGetReservations(Map<String, Object> request) {
-		Table table = dynamoDB.getTable("Reservations");
-		ScanRequest scanRequest = new ScanRequest().withTableName("Reservations");
-		ScanResult result = dynamoDBClient.scan(scanRequest);
-
-		JSONArray reservations = new JSONArray();
-		result.getItems().forEach(item -> reservations.put(new JSONObject(item)));
-
-		Map<String, Object> response = new HashMap<>();
-		response.put("statusCode", 200);
-		response.put("body", new JSONObject().put("reservations", reservations).toString());
-		return response;
+	private Map<String, Object> parseRequestBody(Map<String, Object> event) throws Exception {
+		String body = (String) event.get("body");
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.readValue(body, Map.class);
 	}
 }
